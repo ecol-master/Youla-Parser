10cf698ed6f9e2659aa26545f852ff2c3e0d0ae4
EcolMaster <cloudym444@gmail.com>
2022-06-19 15:32:16 +0500

    main

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from parser.monitors import parse_monitors
+from parser import Parser
+import datetime
+
+
+def get_format_date() -> str:
+   format_date = datetime.datetime.now().strftime("%d_%m_%Y__%H_%M")
+   return format_date
 
 def main():
-   print(parse_monitors())
+   format_date = get_format_date()
+   parser = Parser(format_date=format_date)
+   parser.run_parser()
 
 if __name__ == "__main__":
     main()
