88bc71aefc2014f12e4d0782589329814fbe899f
EcolMaster <cloudym444@gmail.com>
2022-06-19 15:32:37 +0500

    settings and intial module

---
+++ parser/settings.py
@@ -0,0 +1,8 @@
+import json
+
+JSON_POST_REQUESTS_TO_ALL_MONITORS = json.load(
+        open("parser/youla_post_request/request_get_all_monitors.json")
+    )
+
+XLS_FOLDER = "xls_data/"
+YOULA_PREFIX_URL = "https://youla.ru"
